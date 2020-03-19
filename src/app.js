9c6d8918c428734dccde3594a72d500091548315
Adam1824 <forrestadam07@gmail.com>
2020-03-19 10:40:47 +0000

    url encoded middleware attempt

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded(extended, true));
 
 const accountData = fs.readFileSync("src/json/accounts.json", "utf-8");
 const accounts = JSON.parse(accountData);
@@ -38,5 +39,3 @@ app.get("/profile", (req, res) => {
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!")
 });
-
-// hi 
